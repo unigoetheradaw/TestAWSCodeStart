3f5fdad1e77bd91e892edcb4ebbf0665175fc6b2
Robert <Robert@10.108.26.166>
2018-07-06 11:56:36 +0200

    numpy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 def main():
     print("Hallo World")
     with open("test4.txt", "w") as f:
         f.write("Hi")
+    print(np.abs(3.5))
 
 if __name__ == "__main__":
     main()
